b0bd141db8016d03240d291de65c30f0e6d5fa79
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-09-20 09:55:50 +0200

    Added command handler

---
+++ src/handlers/commands.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const { PermissionsBitField } = require('discord.js');
+const { Routes } = require('discord-api-types/v9');
+const { REST } = require('@discordjs/rest');
+
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+
+module.exports = (client) => {
+    const commands = [];
+    fs.readdirSync('./src/commands').forEach(async dir => {
+        const cmdFiles = fs.readdirSync(`./src/commands/${dir}`).filter(file => file.endsWith('.js'));
+
+        for (const file of cmdFiles) {
+            const cmd = require(`./src/commands/${dir}/${file}`);
+            commands.push({
+                name: cmd.name,
+                description: cmd.description,
+                type: cmd.type,
+                options: cmd.options ? cmd.options : null,
+                default_permission: cmd.default_permission ? cmd.default_permission : null,
+                default_member_permissions: cmd.default_member_permissions ? PermissionsBitField.resolve(cmd.default_member_permissions).toString() : null
+            });
+
+            if (!cmd.name) console.log(`Client - Failed to load ${file.split('.js')[0]}`);
+
+        }
+    });
+
+    (async () => {
+        try {
+            await rest.put(process.env.GUILD_ID ?
+                Routes.applicationGuildCommands(process.env.BOT_ID, process.env.GUILD_ID) :
+                Routes.applicationCommands(process.env.BOT_ID),
+                { body: commands }
+            );
+            console.log(`Client - Application (/) commands registered`);
+        } catch (e) {
+            console.log(`Client - Failed to register application (/) commands`, e);
+        }
+    })();
+};
