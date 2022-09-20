55927cdeb2f1df72b8976c5e917310ee85d7a310
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-09-20 10:04:16 +0200

    Added ready event

---
+++ src/events/ready.js
@@ -0,0 +1,18 @@
+const { ActivityType } = require('discord.js');
+const client = require('../..');
+
+client.on('ready', () => {
+    const activityList = [
+        { name: `Fyxren`, type: ActivityType.Watching }
+    ];
+
+    let i = 0;
+    setInterval(() => {
+        if (i >= activityList.length) i = 0;
+        client.user.setActivity(activityList[i]);
+        i++;
+    }, 10000);
+
+    console.log(`Client - Logged in as ${client.user.tag}`);
+
+});
