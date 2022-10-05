81be71edd1f0092ffa9f6077c865f7e8d5bee3f0
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-10-05 20:55:32 +0200

    `Removed` 100% not forgotten buttons collection

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js'
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-client.buttons = new Collection();
 client.embeds = require('./data/config/embeds');
 client.e = require('./data/config/emotes');
 client.c = require('./data/config/colors');
